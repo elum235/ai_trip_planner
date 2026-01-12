3eb3eb92ef42c5dedb2530c34580bf8bc238c9d8
Gary <garydcrenshaw@aol.com>
2026-01-11 20:35:20 -0500

    created end pointwith streamlit frontend

--- app.py
+++
@@ -1,9 +0,0 @@
-from fastapi import FastAPI
-from pydantic import BaseModel
-
-app=FastAPI()
-
-class QueryRequest(BaseModel):
-
-@app.get("/query"):
-async def query_travel_agent(query:QueryRequest):
